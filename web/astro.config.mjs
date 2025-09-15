6afbd5096781eb1820683d1014c810fe2955165e
Zalk0 <88582103+zalk0@users.noreply.github.com>
2025-09-15 19:01:34 +0200

    update packages + add remark for GitHub alerts display
    + fix Hypixel typo

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import { remarkAlert } from "remark-github-blockquote-alert";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkAlert]
+ },
   redirects: {
 	"/discord": "https://discord.com/invite/64pFP94AWA",
   },
